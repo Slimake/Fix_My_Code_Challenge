6cf975594703fe27e0d547805ce638aface3a768
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-22 23:09:35 +0100

    Fix square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -20,8 +20,9 @@ class Square():
     @width.setter
     def width(self, value):
         """ Setter for width """
-        if type(value) is int:
-            self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        self.__width = value
 
     @property
     def height(self):
@@ -31,20 +32,21 @@ class Square():
     @height.setter
     def height(self, value):
         """ Setter for height """
-        if type(value) is int:
-            self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.__width * self.__width
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         """ Perimeter of the square """
-        return (self.__width * 2) + (self.__height * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String representation """
-        return "{}/{}".format(self.__width, self.__height)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
