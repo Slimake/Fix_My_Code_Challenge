ac064a7dce13b85d8db6dcf3bdf87170f8d9b09d
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-22 22:04:01 +0100

    Add documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
+""" square Module """
 
 class square():
+    """ square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +16,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
 
 
